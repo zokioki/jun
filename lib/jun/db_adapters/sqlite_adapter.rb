99981260e6acbd0f0a493aeeb6e4fa8c6f38238e
Zoran <zoran1991@gmail.com>
2016-06-25 18:39:15 -0700

    build basic SQLite adapter

---
+++ lib/jun/db_adapters/sqlite_adapter.rb
@@ -0,0 +1,16 @@
+class SqliteAdapter
+  def initialize
+    require "sqlite3"
+    @db = SQLite3::Database.new(File.dirname(__FILE__) + "/../db/app.db", results_as_hash: true)
+  end
+
+  def execute(sql)
+    @db.execute(sql).each do |row|
+      row.keys.each { |key| row[(key.to_sym rescue key) || key] = row.delete(key) }
+    end
+  end
+
+  def columns(table_name)
+    @db.table_info(table_name).map { |info| info["name"].to_sym }
+  end
+end
