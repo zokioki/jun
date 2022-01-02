e86ee3ca594ca9c3e1ae30ebcb47ead583b233fa
Zoran Pesic <zoran1991@gmail.com>
2022-01-02 14:13:19 -0800

    extract controller render logic to module

--- lib/jun/action_controller/base.rb
+++ lib/jun/action_controller/base.rb
@@ -1,68 +1,16 @@
 # frozen_string_literal: true
 
-require "erubis"
-require "json"
+require_relative "rendering"
 
 module Jun
   module ActionController
     class Base
+      include Jun::ActionController::Rendering
+
       attr_reader :env
 
       def initialize(env)
         @env = env
-      end
-
-      def render(options, extra_options = {})
-        return if @_response_rendered
-
-        template_name = nil
-        if options.is_a?(Symbol) || options.is_a?(String)
-          template_name = options
-          options = extra_options
-        else
-          template_name = options[:template]
-        end
-
-        if content_type = options[:content_type]
-          response.content_type = content_type.to_s
-        end
-
-        if status = options[:status]
-          response.status = status
-        end
-
-        if template_name
-          filepath = views_path.join("#{template_name}.html.erb")
-          template = File.read(filepath)
-          eruby = Erubis::Eruby.new(template)
-
-          body = if options[:locals]
-                   eruby.result(options[:locals].merge(env: env))
-                 else
-                   vars_for_view = {}
-
-                   instance_variables.each do |var_name|
-                     vars_for_view[var_name[1..-1]] = instance_variable_get(var_name)
-                   end
-
-                   eruby.evaluate(vars_for_view)
-                 end
-
-          response.write(body)
-          response.content_type ||= "text/html"
-        elsif options[:text]
-          response.write(options[:text])
-          response.content_type ||= "text/plain"
-        elsif options[:json]
-          json = options[:json].is_a?(String) ? options[:json] : JSON.generate(options[:json])
-          response.write(json)
-          response.content_type ||= "application/json"
-        elsif options[:nothing]
-          response.write("")
-          response.content_type ||= "text/plain"
-        end
-
-        @_response_rendered = true
       end
 
       def request
@@ -75,16 +23,9 @@ module Jun
 
       def handle_response(action)
         public_send(action)
-        render(action) unless @_response_rendered
+        render(action) unless response_rendered?
 
         response
-      end
-
-      private
-
-      def views_path
-        dirname = self.class.name.sub(/Controller\z/, "").underscore
-        Jun.root.join("app/views/#{dirname}")
       end
     end
   end
---
+++ lib/jun/action_controller/rendering.rb
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+
+require "erubis"
+require "json"
+
+module Jun
+  module ActionController
+    module Rendering
+      def render(options, extra_options = {})
+        return if response_rendered?
+
+        template_name = nil
+        if options.is_a?(Symbol) || options.is_a?(String)
+          template_name = options
+          options = extra_options
+        else
+          template_name = options[:template]
+        end
+
+        if content_type = options[:content_type]
+          response.content_type = content_type.to_s
+        end
+
+        if status = options[:status]
+          response.status = status
+        end
+
+        if template_name
+          filepath = views_path.join("#{template_name}.html.erb")
+          template = File.read(filepath)
+          eruby = Erubis::Eruby.new(template)
+
+          body = if options[:locals]
+                   eruby.result(options[:locals].merge(env: env))
+                 else
+                   vars_for_view = {}
+
+                   instance_variables.each do |var_name|
+                     vars_for_view[var_name[1..-1]] = instance_variable_get(var_name)
+                   end
+
+                   eruby.evaluate(vars_for_view)
+                 end
+
+          response.write(body)
+          response.content_type ||= "text/html"
+        elsif options[:text]
+          response.write(options[:text])
+          response.content_type ||= "text/plain"
+        elsif options[:json]
+          json = options[:json].is_a?(String) ? options[:json] : JSON.generate(options[:json])
+          response.write(json)
+          response.content_type ||= "application/json"
+        elsif options[:nothing]
+          response.write("")
+          response.content_type ||= "text/plain"
+        end
+
+        @_response_rendered = true
+      end
+
+      private
+
+      def response_rendered?
+        !!@_response_rendered
+      end
+
+      def views_path
+        dirname = self.class.name.sub(/Controller\z/, "").underscore
+        Jun.root.join("app/views/#{dirname}")
+      end
+    end
+  end
+end
