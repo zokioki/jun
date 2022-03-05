bbc760127a88bf85ce26b6e62d6cce7f0bea5efb
Zoran Pesic <zoran1991@gmail.com>
2022-03-04 23:24:29 -0800

    scaffold out basic cli commands

--- lib/jun/cli.rb
+++ lib/jun/cli.rb
@@ -3,20 +3,26 @@
 module Jun
   module CLI
     class << self
+      COMMAND_MAP = {
+        "new" => :create_new_app,
+        "db:create" => :create_database,
+        "db:migrate" => :run_migrations,
+        "db:drop" => :drop_database,
+        "version" => :version_label
+      }.freeze
+
       def process_command(argv)
         arg = argv.first
+        command = COMMAND_MAP[arg]
+        return if command.nil?
 
-        if arg == "version"
-          puts version_label
-        elsif arg == "new"
-          create_new_app
-        end
+        send(command)
       end
 
       private
 
       def version_label
-        "Jun #{Jun::VERSION}"
+        puts "Jun #{Jun::VERSION}"
       end
 
       def create_new_app
@@ -25,6 +31,18 @@ module Jun
         else
           puts "Creating new app..."
         end
+      end
+
+      def create_database
+        puts "Creating db..."
+      end
+
+      def run_migrations
+        puts "Running migrations..."
+      end
+
+      def drop_database
+        puts "Dropping database..."
       end
     end
   end
