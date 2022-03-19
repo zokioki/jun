7570581be1441d34464508bf9822ac421768d21d
Zoran Pesic <zoran1991@gmail.com>
2022-03-18 22:13:41 -0700

    dry up migrator

--- lib/jun/active_record/migrator.rb
+++ lib/jun/active_record/migrator.rb
@@ -2,18 +2,26 @@
 
 module ActiveRecord
   class Migrator
+    ALLOWED_DIRECTIONS = %w[up down].freeze
+
     def initialize(direction:)
-      @direction = %w[up down].include?(direction.to_s) ? direction.to_s : "up"
+      unless ALLOWED_DIRECTIONS.include?(direction.to_s)
+        raise ArgumentError, "direction must be one of: #{ALLOWED_DIRECTIONS.inspect}"
+      end
+
+      @direction = direction.to_s
     end
 
     def call
-      @direction == "up" ? process_migrations : process_rollback
+      process_migrations!
     end
 
     private
 
-    def process_migrations
-      pending_migration_files.each do |filepath|
+    def process_migrations!
+      files_to_process = up_migration? ? pending_migration_files : processed_migration_files.last(1)
+
+      files_to_process.each do |filepath|
         require filepath
 
         filename = filepath.split("/").last.sub(".rb", "")
@@ -21,28 +29,17 @@ module ActiveRecord
         migration_class_name = filename.split("_", 2).last.camelize
         migration_class = Object.const_get(migration_class_name)
 
-        migration_class.new.up
-        add_to_schema_migrations(migration_version)
+        migration_class.new.public_send(@direction)
+        up_migration? ? add_to_schema_migrations(migration_version) :
+                        remove_from_schema_migrations(migration_version)
 
-        puts "Migration processed (#{filename})."
+        migration_verb = up_migration? ? "processed" : "rolled back"
+        puts "Migration #{migration_verb} (#{filename})."
       end
     end
 
-    def process_rollback
-      filepath = processed_migration_files.last
-      return if filepath.nil?
-
-      require filepath
-
-      filename = filepath.split("/").last.sub(".rb", "")
-      migration_version = filename.split("_").first
-      migration_class_name = filename.split("_", 2).last.camelize
-      migration_class = Object.const_get(migration_class_name)
-
-      migration_class.new.down
-      remove_from_schema_migrations(migration_version)
-
-      puts "Migration rolled back (#{filename})."
+    def up_migration?
+      @direction == "up"
     end
 
     def pending_migration_files
