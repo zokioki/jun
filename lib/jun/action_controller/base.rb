5fa7ddf537be3855098313469547d10f97af4ef8
Zoran Pesic <zoran1991@gmail.com>
2022-03-26 23:56:18 -0700

    consistency

--- lib/jun/action_controller/base.rb
+++ lib/jun/action_controller/base.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-require_relative 'metal'
-require_relative 'callbacks'
+require_relative "metal"
+require_relative "callbacks"
 require_relative "rendering"
 require_relative "redirecting"
 
